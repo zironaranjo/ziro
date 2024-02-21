6b31760af9feec0559dd407207f7eb71f74106e6
ziro <zironaranjo@gmail.com>
2024-02-21 02:33:51 +0100

    gh-pages agregado

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://zironaranjo.github.io/portfolioziro1'
+  base:''
 })
