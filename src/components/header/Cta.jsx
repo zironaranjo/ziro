b212653a9424f358a9f898e7a88c09dddc10ea86
ziro <zironaranjo@gmail.com>
2024-03-04 16:20:16 +0100

    frase

--- src/components/header/Cta.jsx
+++ src/components/header/Cta.jsx
@@ -7,7 +7,7 @@ import { RiMegaphoneLine } from "react-icons/ri";
 const Cta = () => {
   return (
     <div className='cta'>
-      <a href={CV} download className='btn'> C.vitae <PiDownloadSimpleBold /></a>
+      <a href={CV} download className='btn'><PiDownloadSimpleBold />&nbsp C.vitae </a>
       <a href="#contact" className='btn btn-primary'>Talk m'   <RiMegaphoneLine /></a>
     </div>
   )
