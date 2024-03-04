cdcf9472f7aa158d328d681dbfd3306aa9092765
ziro <zironaranjo@gmail.com>
2024-03-04 21:45:57 +0100

    redes

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -7,8 +7,9 @@ import { FaGithub } from "react-icons/fa"
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-     <a href="https://linkedin.com" target="_blank" rel="noreferrer"><FaLinkedin /></a>
-     <a href="https://github.com" target="_blank" rel="noreferrer"><FaGithub /></a>
+     <a href="https://www.linkedin.com/in/ziro-naranjo-640109183?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank" rel="noreferrer"><FaLinkedin /></a>
+
+     <a href="https://github.com/zironaranjo" target="_blank" rel="noreferrer"><FaGithub /></a>
       
       
     </div>
